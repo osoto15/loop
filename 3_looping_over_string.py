6bc0c3dac1b23e062fa10607d8108c809f097293
osoto15 <osoto15@cps.edu>
2025-12-11 08:37:50 -0600

    Add vowel counting feature to string loop

--- 3_looping_over_string.py
+++ 3_looping_over_string.py
@@ -1,6 +1,16 @@
 # Example Practice:
 # Ask the user for a word.
+word = input("Provide a word: ")
+
 # Use a for loop to print each letter on a new line.
+for letter in word:
+    print(letter)
 
 # Challenge:
 # Count how many vowels are in the word.
+vowels = "aeiouAEIOU"
+count = 0
+for letter in word:
+    if letter in vowels:
+        count += 1
+print("numbers of vowels,", count)
